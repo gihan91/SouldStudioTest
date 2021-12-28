5e9e7d19af9db832f1be775ed06f2e56a304e806
mac <mac@macs-MacBook-Pro.local>
2021-12-28 18:41:28 +0530

    Test commit

---
+++ SoulStudioApp/SoulStudioApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoulStudioApp
+//
+//  Created by mac on 28/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
