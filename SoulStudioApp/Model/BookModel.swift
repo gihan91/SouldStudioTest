5f505bb3883109c876dd7c362d4732ef6b2141fa
mac <mac@macs-MacBook-Pro.local>
2021-12-28 19:25:38 +0530

    Completed Model class

--- SoulStudioApp/Model/BookModel.swift
+++ SoulStudioApp/Model/BookModel.swift
@@ -7,12 +7,23 @@
 
 import Foundation
 
+//MARK: - BOOK MODEL
 struct Book: Codable {
     let kind: String?
     let totalItems: Int
     let items: [Item]
 }
 
-struct Items: Codable {
-    
+struct Item: Codable {
+    let volumeInfo: VolumeInfo?
+}
+
+struct VolumeInfo: Codable {
+    let title: String?
+    let description: String?
+    let imageLinks: ImageLinks?
+}
+
+struct ImageLinks: Codable {
+    let smallThumbnail, thumbnail: String
 }
